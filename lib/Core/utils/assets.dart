4ea4d53263cca7d245b39f5d0b8561a9b3bd669a
alikasrawy <kasrawyali47@gmail.com>
2025-08-11 21:53:11 +0300

    create assets class

---
+++ lib/Core/utils/assets.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  static const String imagesBookImg = "assets/images/book_img.png";
+  static const String imagesCloud = "assets/images/cloud.png";
+  static const String imagesCosmetics = "assets/images/cosmetics.png";
+  static const String imagesElectronics = "assets/images/electronics.png";
+  static const String imagesEmptySearch = "assets/images/empty_search.png";
+  static const String imagesError = "assets/images/error.png";
+  static const String imagesFashion = "assets/images/fashion.png";
+  static const String imagesMobiles = "assets/images/mobiles.png";
+  static const String imagesOrder = "assets/images/order.png";
+  static const String imagesPc = "assets/images/pc.png";
+  static const String imagesRoundedMap = "assets/images/rounded_map.png";
+  static const String imagesShoes = "assets/images/shoes.png";
+  static const String imagesShoppingCart = "assets/images/shopping_cart.png";
+  static const String imagesWarning = "assets/images/warning.png";
+  static const String imagesWatch = "assets/images/watch.png";
+}
+
