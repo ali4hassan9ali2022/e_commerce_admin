a366bc3e55d6a74b79a33ce15674741c505cbcb8
alikasrawy <kasrawyali47@gmail.com>
2025-08-11 21:49:44 +0300

    create my app

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const EcommerceAdmin());
+}
+
+class EcommerceAdmin extends StatelessWidget {
+  const EcommerceAdmin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
