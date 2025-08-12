458a62ea00e59762d1a6be8ee47df46d9fb5d126
alikasrawy <kasrawyali47@gmail.com>
2025-08-12 16:36:42 +0300

    create dashboard model

---
+++ lib/Models/dashboard_model.dart
@@ -0,0 +1,7 @@
+class DashboardModel {
+  final String title;
+  final String imagePath;
+  final Function function;
+
+  DashboardModel({required this.title, required this.imagePath, required this.function});
+}
