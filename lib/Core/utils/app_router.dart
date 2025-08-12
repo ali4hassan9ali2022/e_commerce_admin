f5f35b530d02e50be4d44de9d51abd38e3aceb75
alikasrawy <kasrawyali47@gmail.com>
2025-08-12 16:47:36 +0300

    create search view

--- lib/Core/utils/app_router.dart
+++ lib/Core/utils/app_router.dart
@@ -1,8 +1,13 @@
 import 'package:ecommerce_admin/Views/dashboard_view.dart';
+import 'package:ecommerce_admin/Views/search_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
+  static const String kSearchView = "kSearchView";
   static final router = GoRouter(
-    routes: [GoRoute(path: "/", builder: (context, state) => DashboardView())],
+    routes: [
+      GoRoute(path: "/", builder: (context, state) => DashboardView()),
+      GoRoute(path: kSearchView, builder: (context, state) => SearchView()),
+    ],
   );
 }
